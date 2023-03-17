9f0dd4abf1b0bd07d9d5ba9f10be19cf45a480bd
piupaulgit <contactpiu619@gmail.com>
2023-03-17 23:49:06 +0530

    navigation container added

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,9 +1,11 @@
 const logo1 =  require("./logo1.jpg");
 const logo2 =  require("./logo2.png");
 const login =  require("./login.png");
+const circle = require("./circle.png");
 
 export {
     logo1,
     logo2,
-    login
+    login,
+    circle
 }
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,19 +1,58 @@
 import React from 'react'
-import { Button, Image, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import { login } from '../assets/images';
 const appStyles = require('../App.scss')
 
+const smallCircleLeftVal = '-10%';
+const smallCircleTopVal = '1%';
+const bigCircleLeftVal = '50%';
+const bigCircleTopVal = '50%';
+
 const Login = () => {
   return (
-    <View>
-        <View>
-            <Image source={login} style={{width: 134, height: 426}}/>
+    <View style={styles.loginScreen}>
+        <View style={styles.smallCircle}/>
+        <View style={styles.bigCircle}/>
+        <View style={styles.loginImage}>
+            <Image source={login} style={{width: 178, height: 392}}/>
         </View>
         <Text style={appStyles.h1}>Manage your buddies to calculate</Text>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</Text>
+        <Text style={appStyles.bodyCopy}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</Text>
         <Button title='Login With Google'/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  loginScreen: {
+    position:'relative',
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  loginImage:{
+    display: 'flex',
+    alignItems: 'center'
+  },
+  smallCircle:{
+    height: 194,
+    width: 194,
+    backgroundColor: '#D9D9D9',
+    opacity: .2,
+    position: 'absolute',
+    borderRadius: 194,
+    top: smallCircleTopVal,
+    left: smallCircleLeftVal
+  },
+  bigCircle:{
+    height: 300,
+    width: 300,
+    backgroundColor: '#D9D9D9',
+    opacity: .2,
+    position: 'absolute',
+    borderRadius: 194,
+    top: bigCircleTopVal,
+    left: bigCircleLeftVal
+  }
+});
 
 export default Login;
